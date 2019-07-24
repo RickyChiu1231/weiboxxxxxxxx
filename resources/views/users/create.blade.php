ffc5a54d71dbd9d0bf4b3a97a638b1363eedcdf0
RickyChiu1231 <laiyin101020@gmail.com>
2019-07-24 02:41:10 +0000

    增加CSRF驗證

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
-@section('title', 'Register')
+@section('title', 'Sign Up')
 
 @section('content')
 <div class="offset-md-2 col-md-8">
@@ -9,27 +9,29 @@
     </div>
     <div class="card-body">
       <form method="POST" action="{{ route('users.store') }}">
-          <div class="form-group">
-            <label for="name">Name：</label>
-            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
-          </div>
+        {{ csrf_field() }}
 
-          <div class="form-group">
-            <label for="email">Email：</label>
-            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-          </div>
+        <div class="form-group">
+          <label for="name">Name：</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password">Password：</label>
-            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-          </div>
+        <div class="form-group">
+          <label for="email">Email：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password_confirmation">Confirm Password：</label>
-            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-          </div>
+        <div class="form-group">
+          <label for="password">Password：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
 
-          <button type="submit" class="btn btn-primary">Sign Up</button>
+        <div class="form-group">
+          <label for="password_confirmation">Confirm Password：</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Sign Up</button>
       </form>
     </div>
   </div>
