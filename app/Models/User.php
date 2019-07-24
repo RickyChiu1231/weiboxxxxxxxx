5811725a0d30c91ff3c9cde0e508ddfc9e00c8f0
RickyChiu1231 <laiyin101020@gmail.com>
2019-07-24 02:23:58 +0000

    userregistrationform

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
 
     public function gravatar($size = '100')
     {
-        $hash = md5(strtolower(trim($this->attributes['email'])));
+        $hash = md5(strtolower(trim($this->attributes['name'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
